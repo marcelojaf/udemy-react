506fbfd1b129833de6d9d82e810dc4a32c2fb655
Marcelo Amador <marcelojaf@gmail.com>
2022-10-07 09:46:12 -0300

    Finished New Expense form

---
+++ expenses-tracker/src/components/Expenses/Expenses.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import ExpensesList from "./ExpensesList";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  function filterChangeHandler(selectedYear) {
+    setFilteredYear(selectedYear);
+  }
+
+  const fileteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onFilterChange={filterChangeHandler}
+      />
+      <ExpensesList items={fileteredExpenses} />
+    </Card>
+  );
+}
+
+export default Expenses;
